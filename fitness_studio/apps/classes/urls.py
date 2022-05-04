638e6e4aa8adaf221e901f93ed170d997dd85e4c
Reinier Bullain Escobar <reinier.bullain-escobar@epita.fr>
2022-05-04 18:51:31 +0200

    Starting booking view. Using allauth utilities for redirections.

--- fitness_studio/apps/classes/urls.py
+++ fitness_studio/apps/classes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.classes.views import ClassListView, ClassDetailView
+from apps.classes.views import ClassDescriptionListView, ClassDescriptionDetailView, ClassBookingView
 
 app_name = 'classes'
 
 urlpatterns = [
-    path('', ClassListView.as_view(), name='class-list'),
-    path('<slug:slug>/', ClassDetailView.as_view(), name='class-detail'),
+    path('', ClassDescriptionListView.as_view(), name='class-list'),
+    path('<slug:slug>/', ClassDescriptionDetailView.as_view(), name='class-detail'),
+    path('<slug:cdslug>/<int:pk>/book/', ClassBookingView.as_view(), name='class-booking'),
 ]
--- fitness_studio/apps/classes/views.py
+++ fitness_studio/apps/classes/views.py
@@ -1,13 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
-from apps.classes.models import ClassDescription
+from apps.classes.models import ClassDescription, ClassInstance
 
 
-class ClassDetailView(DetailView):
+class ClassDescriptionListView(ListView):
+    model = ClassDescription
+    template_name = 'classes/class-list.html'
+
+
+class ClassDescriptionDetailView(DetailView):
     model = ClassDescription
     template_name = 'classes/class-detail.html'
 
 
-class ClassListView(ListView):
-    model = ClassDescription
-    template_name = 'classes/class-list.html'
+class ClassBookingView(LoginRequiredMixin, DetailView):
+    model = ClassInstance
+    template_name = 'classes/class-booking.html'
