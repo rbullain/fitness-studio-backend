a55c48876bababf1942510ea1ca6b8ebfc35ec41
Reinier Bullain <r.bullain94@gmail.com>
2022-09-24 12:23:52 +0200

    Adding tests for User account.

---
+++ tests/apps/accounts/test_user_creation.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+
+class UserCreationTestCase(TestCase):
+    UserModel = get_user_model()
+
+    def test_create_user(self):
+        """Test if a user is created successfully."""
+        email = 'user@email.com'
+        password = 'foo'
+
+        user_obj = self.UserModel.objects.create_user(email=email, password=password)
+
+        self.assertEqual(user_obj.email, email)
+        self.assertTrue(user_obj.check_password(password))
+        self.assertTrue(user_obj.is_active)
+
+        self.assertFalse(user_obj.is_staff)
+
+    def test_create_user_with_valid_email_normalizes_email(self):
+        """Test if the email is normalized."""
+        email = 'USeR@EmAil.COM'
+
+        user_obj = self.UserModel.objects.create_user(email=email, password='foo')
+
+        self.assertEqual(user_obj.email, self.UserModel.objects.normalize_email(email))
+
+    def test_create_superuser(self):
+        """Test if a superuser is created successfully."""
+        user_obj = self.UserModel.objects.create_superuser(email='superuser@email.com', password='foo')
+
+        self.assertTrue(user_obj.is_superuser)
+        self.assertTrue(user_obj.is_staff)
