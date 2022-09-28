496c42d6a84c3fd4c4ec395d96f794feba440ec2
Reinier Bullain <r.bullain94@gmail.com>
2022-09-28 20:48:39 +0200

    Initial API definition.

--- fitness_studio/api/classes/urls.py
+++ fitness_studio/api/classes/urls.py
@@ -3,6 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from api.classes import views
 
-urlpatterns = [
+router = DefaultRouter()
+router.register('categories', views.ClassCategoryViewSet, basename='class-category')
+router.register('descriptions', views.ClassDescriptionViewSet, basename='class-description')
+router.register('schedules', views.ClassScheduleViewSet, basename='class-schedule')
+router.register('instances', views.ClassInstanceViewSet, basename='class-instance')
 
+urlpatterns = [
+    path('', include(router.urls)),
 ]
--- fitness_studio/api/classes/views.py
+++ fitness_studio/api/classes/views.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets
 
-from api.classes.serializers import ClassInstanceSerializer
-from apps.classes.models import ClassInstance
+from apps.classes.models import ClassCategory, ClassDescription, ClassSchedule, ClassInstance
+from api.classes import serializers
+
+
+class ClassCategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ClassCategory.objects.all()
+    serializer_class = serializers.ClassCategorySerializer
+
+
+class ClassDescriptionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ClassDescription.objects.all()
+    serializer_class = serializers.ClassDescriptionSerializer
+
+
+class ClassScheduleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ClassSchedule.objects.all()
+    serializer_class = serializers.ClassScheduleSerializer
 
 
 class ClassInstanceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ClassInstance.objects.all()
-    serializer_class = ClassInstanceSerializer
+    serializer_class = serializers.ClassInstanceSerializer
