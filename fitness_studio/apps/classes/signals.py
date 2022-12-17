1c437aad7859809b23c7ef8070d0aaa3b37ea470
Reinier Bullain <r.bullain94@gmail.com>
2022-12-17 16:03:40 +0100

    Using bulk create to create new missing ClassInstances on ClassScheduleUpdate.

--- fitness_studio/apps/classes/signals.py
+++ fitness_studio/apps/classes/signals.py
@@ -14,7 +14,7 @@ def on_class_schedule_update(instance: ClassSchedule, created, **kwargs):
 
     if not created:
         # Delete classes that do not belong to the schedule weekdays
-        dj_weekdays = [(wd.weekday + 1) % 7 + 1 for wd in weekdays]     # Django weekdays starts from the Sunday with value 1
+        dj_weekdays = [(wd.weekday + 1) % 7 + 1 for wd in weekdays]  # Django weekdays starts from Sunday with value 1
         instance.classes.exclude(start_datetime__week_day__in=dj_weekdays).delete()
 
         # Delete classes that are not in the date interval
@@ -34,16 +34,16 @@ def on_class_schedule_update(instance: ClassSchedule, created, **kwargs):
         ClassInstance.objects.bulk_update(updated_classes_instances, fields_to_update)
 
     # Create class instances that are missing
-    for dt in rrule(WEEKLY, dtstart=instance.start_date, until=instance.end_date, byweekday=weekdays):
-        start_datetime = datetime.combine(dt, instance.start_time, timezone.get_current_timezone())
-        end_datetime = datetime.combine(dt, instance.end_time, timezone.get_current_timezone())
+    unique_fields = ['class_schedule', 'start_datetime', 'end_datetime']
+    create_classes_instances = [
+        ClassInstance(
+            class_description=instance.class_description,
+            class_schedule=instance,
+            start_datetime=datetime.combine(dt, instance.start_time, timezone.get_current_timezone()),
+            end_datetime=datetime.combine(dt, instance.end_time, timezone.get_current_timezone()),
+            location=instance.location,
+            room=instance.room,
+        ) for dt in rrule(WEEKLY, dtstart=instance.start_date, until=instance.end_date, byweekday=weekdays)
+    ]
 
-        cl_obj, _ = instance.classes.get_or_create(
-            start_datetime=start_datetime,
-            end_datetime=end_datetime,
-            defaults={
-                'class_description': instance.class_description,
-                'location': instance.location,
-                'room': instance.room,
-            }
-        )
+    ClassInstance.objects.bulk_create(create_classes_instances, unique_fields=unique_fields)
