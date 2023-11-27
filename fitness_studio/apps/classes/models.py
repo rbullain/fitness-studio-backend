8c7108eed0c7dd2180a025db04a96bb236eaa4d4
Reinier Bullain Escobar <reinier.bullain-escobar@epita.fr>
2023-11-27 21:54:55 +0100

    Removing default value from image. This should be handled in the frontend.

--- fitness_studio/apps/classes/models.py
+++ fitness_studio/apps/classes/models.py
@@ -8,11 +8,8 @@ from dateutil.rrule import MO, TU, WE, TH, FR, SA, SU
 
 class ClassCategory(models.Model):
     """The category of a class."""
-    DEFAULT_CATEGORY_IMAGE_PATH = 'images/categories/default.png'
-
     name = models.CharField(_('name'), max_length=50)
-    image = VersatileImageField(_('image'), upload_to='images/categories/', default=DEFAULT_CATEGORY_IMAGE_PATH,
-        null=True, blank=True)
+    image = VersatileImageField(_('image'), upload_to='images/categories/', null=True, blank=True)
 
     class Meta:
         verbose_name_plural = _("class categories")
